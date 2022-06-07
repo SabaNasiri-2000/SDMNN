c31941535724ba34403996a2b5097edfaff287c1
Saba <you@example.com>
2022-06-07 18:44:18 -0400

    changing root and file system

--- problem2/My_cli.sh
+++ problem2/My_cli.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
-sudo unshare --fork --pid --mount-proc -n -u -m bash -c "hostname saba && /bin/bash"
+a=$(date)
+PART=(${a// / })
 
+sudo mkdir ${PART[4]}
+sudo tar -zxf ubuntu-focal-oci-amd64-root.tar.gz -C ./${PART[4]}
 
+sudo unshare --fork --pid --mount-proc -n -u -m --root=./${PART[4]} bash -c "hostname $1 && /bin/bash"
+
+sudo rm -r ${PART[4]}
 
 
 
