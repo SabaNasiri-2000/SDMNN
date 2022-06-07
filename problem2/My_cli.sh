f12cae0e7654aa0181ff6f30f3dfbf96623ff7de
Saba <you@example.com>
2022-06-07 18:42:18 -0400

    creating different pid and mount namespaces

--- problem2/My_cli.sh
+++ problem2/My_cli.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-sudo touch /root/uts-ns
-sudo touch /root/net-ns
 
 
-sudo unshare --net=/root/net-ns --uts=/root/uts-ns hostname saba
+sudo unshare --fork --pid --mount-proc -n -u -m bash -c "hostname saba && /bin/bash"
 
 
-sudo nsenter --uts=/root/uts-ns --net=/root/net-ns /bin/bash
 
-sudo umount /root/uts-ns
-sudo umount /root/net-ns
+
 
