e2388693e040d3e2130e5864cdfef63413b72d7d
Saba <you@example.com>
2022-06-11 10:58:37 -0400

    changing server for responsing to every arbitary massage which is sent by post

--- problem3/httpserver.py
+++ problem3/httpserver.py
@@ -1,60 +1,40 @@
 
 from http.server import HTTPServer , BaseHTTPRequestHandler
 import json
+import socketserver
 
-isok=True
+massage="ok"
 
 class WebServerHandler(BaseHTTPRequestHandler):
        
     def do_GET(self):
-        global isok
-        if self.path=='/api/v1/status':   
-        	if isok==True:
-            		self.send_response(200)
-            		self.send_header('Content-type', 'application/json')
-            		self.end_headers()
-            		response= {"status":'ok'}
-            		self.wfile.write(bytes(json.dumps(response, 	ensure_ascii=False), 'utf-8'))
-        	else:
-            		self.send_response(201)
-            		self.send_header('Content-type', 'application/json')
-            		self.end_headers()
-            		response= {"status":'not ok'}
-            		self.wfile.write(bytes(json.dumps(response, ensure_ascii=False), 'utf-8'))
-           
+    	global massage
+    	if self.path=='/api/v1/status':   
+            	self.send_response(200)
+            	self.send_header('Content-type', 'application/json')
+            	self.end_headers()
+            	response= {"status":massage}
+            	self.wfile.write(bytes(json.dumps(response,ensure_ascii=False), 'utf-8'))
+        	
        
     def do_POST(self):
-        global isok
-        if self.path=='/api/v1/status':
+    	global massage
+    	if self.path=='/api/v1/status':
         	content_length = int(self.headers['Content-Length'])
         	post_data = self.rfile.read(content_length)
-        	s=post_data.decode('UTF-8');
-        	a=s.find("not ok")
-        	if a>=0:
-            		response = {
-               	 	'status':'not ok'
-               	 	}
-            		isok=False
-            		self.send_response(201)
-            		self.send_header('Content-type', 'application/json')
-            		self.end_headers()
-            		self.wfile.write(bytes(json.dumps(response, 				ensure_ascii=False), 'utf-8'))
-        	else:
-            		response = {
-               		 'status':'ok'
-               		 }
-            		isok=True
-            		self.send_response(200)
-            		self.send_header('Content-type', 'application/json')
-            		self.end_headers()
-            		self.wfile.write(bytes(json.dumps(response, 				ensure_ascii=False), 'utf-8'))
-       
-        	return
+        	data=json.loads(post_data)
+        	massage=data["status"]
+        	self.send_response(201)
+        	self.send_header('Content-type', 'application/json')
+        	self.end_headers()
+        	response=json.dumps({"status":massage})
+        	self.wfile.write(bytes(response,'utf-8'))
+        	
+        
 	
-
 with HTTPServer(('',8000),WebServerHandler) as server:
 	server.serve_forever()
-
+	
 #port = 8000
 #server = HTTPServer(('localhost', port), WebServerHandler)
 #print("Web server is running on port {}".format(port))
